929ee3aad5427063252dcac5c5288e476d9eeaee
Palwisha Akhtar <palwishaakhtar@gmail.com>
2021-01-17 12:00:55 +0100

    initial commit

---
+++ test.py
@@ -0,0 +1,189 @@
+import os
+import cv2
+import glob
+import matplotlib.pyplot as plt
+import random
+from PIL import Image
+from os import listdir
+from os.path import isfile, join
+import torchvision 
+import torch 
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from utils import get_path, parse_xml, create_directories, load_checkpoint, save_loss_fig, save_accuracy_fig
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+mean = [0.6583, 0.4580, 0.0877]
+# mean = [0.485, 0.456, 0.406]
+std = [0.2412, 0.2313, 0.2387]
+# std = [0.229, 0.224, 0.225] 
+BATCH_SIZE = 16
+LEARNING_RATE = 1e-4
+NUM_EPOCH = 30
+WEIGHT_DECAY = 0
+
+# create_directories()
+
+model = models.mobilenet_v2()
+# model = models.resnet50()
+# model = models.vgg19()
+# model = models.densenet161()
+# model = models.resnet34(pretrained=True)
+#for layer, param in model.named_parameters():
+#    if 'fc' not in layer:
+#        param.requires_grad = False
+
+#n_inputs = model.fc.in_features
+#last_layer = torch.nn.Linear(n_inputs, 3)
+#model.fc.out_features = last_layer
+model.classifier._modules['1'] = torch.nn.Linear(1280, 3)
+# model.classifier = torch.nn.Linear(2208,3)
+if torch.cuda.device_count() > 1:
+  print("Let's use" + str(torch.cuda.device_count()) + "GPUs!")
+  model = torch.nn.DataParallel(model)
+train_transforms = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor(),transforms.Normalize(mean=mean,std=std)])
+
+dataset = datasets.ImageFolder("train", transform = train_transforms)
+
+dataset_size = len(dataset)
+train_size = int(dataset_size * 0.6)
+val_size = int(dataset_size * 0.2)
+test_size = dataset_size - train_size - val_size
+
+train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])
+
+print(f'Dataset size: {dataset_size}\nTrain set size: {len(train_dataset)}\nValidation set size: {len(val_dataset)}\nTest set size: {len(test_dataset)}')
+
+loader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+
+model.to(device)
+
+best_epoch = 0
+training_losses, val_losses, training_accuracies, validation_accuracies = [], [], [], []
+
+for epoch in range(NUM_EPOCH):
+    epoch_train_loss = 0
+    correct = 0
+    total = 0
+
+    model.train()
+    for X, y in train_loader:
+        X, y = X.to(device), y.to(device)
+        
+        optimizer.zero_grad()
+        result = model(X)
+        loss = criterion(result, y)
+        epoch_train_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+
+        _, maximum = torch.max(result.data, 1)
+        total += y.size(0)
+        correct += (maximum == y).sum().item()
+    
+    training_accuracy = correct / total
+    training_losses.append(epoch_train_loss)
+    training_accuracies.append(training_accuracy)
+
+    epoch_val_loss = 0
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        model.eval()
+        for X, y in val_loader:
+            X, y = X.to(device), y.to(device)
+             
+            result = model(X)
+            loss = criterion(result, y)
+            epoch_val_loss += loss.item()
+            _, maximum = torch.max(result.data, 1)
+            total += y.size(0)
+            correct += (maximum == y).sum().item()
+            
+    val_losses.append(epoch_val_loss)
+    accuracy = correct/total
+    validation_accuracies.append(accuracy)
+    print(f'EPOCH:{epoch}, Training Loss:{epoch_train_loss}, Validation Loss:{epoch_val_loss}, Training Accuracy: {training_accuracy}, Validation Accuracy: {accuracy}')
+    
+    if min(val_losses) == val_losses[-1]:
+        best_epoch = epoch
+        checkpoint = {'model': model, 'state_dict': model.state_dict(), 'optimizer' : optimizer.state_dict()}
+
+        torch.save(checkpoint, "models/" + f'{epoch}.pth')
+        print("Model saved")
+
+save_loss_fig(NUM_EPOCH, training_losses, val_losses)
+save_accuracy_fig(NUM_EPOCH, training_accuracies, validation_accuracies)
+# plt.show()
+
+filepath = f'models/{best_epoch}.pth'
+loaded_model = load_checkpoint(filepath)
+
+correct = 0
+total = 0
+    
+with torch.no_grad():
+    for X, y in test_loader:
+        X, y = X.to(device), y.to(device)
+
+        result = loaded_model(X)
+        _, maximum = torch.max(result.data, 1)
+        print(maximum)
+        total += y.size(0)
+        correct += (maximum == y).sum().item()
+
+accuracy = correct/total
+print(f'Testing Accuracy: {accuracy}')
+
+""" cap = cv2.VideoCapture(0)
+
+font_scale, thickness = 1, 2
+red,green,blue = (0,0,255), (0,255,0), (255,0,0)
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+rectangle_spec = {0: ("Masked", green), 1: ("No Mask", red), 2: ("Incorrect Mask", blue)}
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.4, 4)
+        
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), blue, 2)
+            
+            croped_img = frame[y:y+h, x:x+w]
+            pil_image = Image.fromarray(croped_img, mode = "RGB")
+            pil_image = train_transforms(pil_image)
+            image = pil_image.unsqueeze(0)
+            
+            result = loaded_model(image)
+            _, maximum = torch.max(result.data, 1)
+            prediction = maximum.item()
+            print(prediction)
+            
+            text, color = rectangle_spec[prediction]
+            cv2.putText(frame, text, (x,y - 10), font, font_scale, color, thickness)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
+        
+        cv2.imshow('frame',frame)
+        
+        if (cv2.waitKey(1) & 0xFF) == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows() """
---
+++ utils.py
@@ -0,0 +1,125 @@
+import os
+import glob
+import xmltodict
+import cv2
+import torch
+import matplotlib.pyplot as plt
+
+classes = {"with_mask": 0, "without_mask": 1, "mask_weared_incorrect": 2}
+IMAGE_DIR = 'dataset/images/'
+ANNOTATION_DIR = 'dataset/annotations/'
+TRAIN_DIR = 'train/'
+
+def get_image_names():
+    paths = glob.glob("dataset/images/*")
+    image_names = [path.rsplit("/",1)[1] for path in paths]
+    
+    return image_names
+
+def get_paths(image_name):
+    image_path = IMAGE_DIR + image_name
+    xml_name = image_name[:-4] + '.xml'
+    annotation_path = ANNOTATION_DIR + xml_name
+        
+    return  image_path, annotation_path
+
+def parse_xml(annotation_path):
+    xml = xmltodict.parse(open(annotation_path , 'rb'))
+    item_list = xml['annotation']['object']
+    
+    # when image has only one bounding box
+    if not isinstance(item_list, list):
+        item_list = [item_list]
+        
+    result = []
+    
+    for item in item_list:
+        name = item['name']
+        bndbox = [(int(item['bndbox']['xmin']), int(item['bndbox']['ymin'])),
+                  (int(item['bndbox']['xmax']), int(item['bndbox']['ymax']))]       
+        result.append((name, bndbox))
+    
+    return result
+    
+def create_directory(dirname):
+    try:
+        os.mkdir(dirname)
+    except FileExistsError:
+        pass
+        # print("Directory " + dirname + " already exists.")
+
+def create_directories():
+    directories = [TRAIIN_DIR + "0/", TRAIN_DIR + "1/", TRAIN_DIR + "2/", "models/"]
+
+    for directory in directories:
+        create_directory(directory)
+
+def crop_image(image_name):
+    image_path, annotation_path = get_paths(image_name)
+
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    labels = parse_xml(annotation_path)
+    
+    cropped_pairs = []
+    for name, bndbox in labels:
+        cropped_image = image[bndbox[0][1]:bndbox[1][1], bndbox[0][0]:bndbox[1][0]]
+        label = classes[name]
+        
+        croppped_pair = [cropped_image, label]
+        cropped_pairs.append(cropped_pair)
+        
+    return cropped_pairs
+
+def prepare_dataset():
+    image_names = get_image_names()
+    
+    for image_name in image_names:
+        cropped_pairs = crop_image(image_name)
+        
+        for index, img, label in enumerate(cropped_pairs):
+            cropped_img_name = str(index) + ".jpg"
+            cv2.imwrite(TRAIN_DIR + str(label) + "/" + cropped_img_name, img)
+
+def load_checkpoint(filepath):
+    checkpoint = torch.load(filepath)
+    model = checkpoint['model']
+    model.load_state_dict(checkpoint['state_dict'])
+    for parameter in model.parameters():
+        parameter.requires_grad = False
+    
+    return model.eval()
+
+def get_mean_std(loader):
+    channels_sum, channels_squared_sum, num_batches = 0,0,0
+
+    for data, _ in loader:
+        channels_sum += torch.mean(data, dim=[0,2,3])
+        channels_squared_sum += torch.mean(data ** 2, dim=[0,2,3])
+        num_batches +=1
+
+    mean = channels_sum / num_batches
+    std = (channels_squared_sum/num_batches - mean ** 2) ** 0.5
+
+    return mean,std
+
+def save_loss_fig(NUM_EPOCH, training_losses, val_losses):
+    plt.figure(1)
+    plt.plot(range(NUM_EPOCH), training_losses, label='Training')
+    plt.plot(range(NUM_EPOCH), val_losses, label='Validation')
+    plt.title("Training Loss vs Validation Loss")
+    plt.xlabel("Epoch Number")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.savefig("losses")
+
+def save_accuracy_fig(NUM_EPOCH, training_accuracies, val_accuracies):
+    plt.figure(2)
+    plt.plot(range(NUM_EPOCH), training_accuracies, label='Training')
+    plt.plot(range(NUM_EPOCH), val_accuracies, label='Validation')
+    plt.title("Training Accuracy vs Validation Accuracy")
+    plt.xlabel("Epoch Number")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.savefig("accuracies")
