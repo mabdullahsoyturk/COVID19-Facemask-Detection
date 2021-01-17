ca2f80bd224f2d41326508dbbaf2343f28661809
mabdullahsoyturk <muhammetabdullah.soyturk@agu.edu.tr>
2021-01-17 12:17:46 +0100

    add test function

--- test.py
+++ test.py
@@ -14,9 +14,7 @@ from utils import get_path, parse_xml, create_directories, load_checkpoint, save
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 mean = [0.6583, 0.4580, 0.0877]
-# mean = [0.485, 0.456, 0.406]
 std = [0.2412, 0.2313, 0.2387]
-# std = [0.229, 0.224, 0.225] 
 BATCH_SIZE = 16
 LEARNING_RATE = 1e-4
 NUM_EPOCH = 30
@@ -39,10 +37,9 @@ model = models.mobilenet_v2()
 model.classifier._modules['1'] = torch.nn.Linear(1280, 3)
 # model.classifier = torch.nn.Linear(2208,3)
 if torch.cuda.device_count() > 1:
-  print("Let's use" + str(torch.cuda.device_count()) + "GPUs!")
   model = torch.nn.DataParallel(model)
-train_transforms = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor(),transforms.Normalize(mean=mean,std=std)])
 
+train_transforms = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor(),transforms.Normalize(mean=mean,std=std)])
 dataset = datasets.ImageFolder("train", transform = train_transforms)
 
 dataset_size = len(dataset)
@@ -122,66 +119,69 @@ for epoch in range(NUM_EPOCH):
 
 save_loss_fig(NUM_EPOCH, training_losses, val_losses)
 save_accuracy_fig(NUM_EPOCH, training_accuracies, validation_accuracies)
-# plt.show()
 
 filepath = f'models/{best_epoch}.pth'
 loaded_model = load_checkpoint(filepath)
 
-correct = 0
-total = 0
-    
-with torch.no_grad():
-    for X, y in test_loader:
-        X, y = X.to(device), y.to(device)
-
-        result = loaded_model(X)
-        _, maximum = torch.max(result.data, 1)
-        print(maximum)
-        total += y.size(0)
-        correct += (maximum == y).sum().item()
-
-accuracy = correct/total
-print(f'Testing Accuracy: {accuracy}')
-
-""" cap = cv2.VideoCapture(0)
-
-font_scale, thickness = 1, 2
-red,green,blue = (0,0,255), (0,255,0), (255,0,0)
-font = cv2.FONT_HERSHEY_SIMPLEX
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-rectangle_spec = {0: ("Masked", green), 1: ("No Mask", red), 2: ("Incorrect Mask", blue)}
-
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.4, 4)
+def test(model, test_loader):
+    correct, total = 0, 0
         
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), blue, 2)
-            
-            croped_img = frame[y:y+h, x:x+w]
-            pil_image = Image.fromarray(croped_img, mode = "RGB")
-            pil_image = train_transforms(pil_image)
-            image = pil_image.unsqueeze(0)
-            
-            result = loaded_model(image)
+    with torch.no_grad():
+        for X, y in test_loader:
+            X, y = X.to(device), y.to(device)
+
+            result = model(X)
             _, maximum = torch.max(result.data, 1)
-            prediction = maximum.item()
-            print(prediction)
-            
-            text, color = rectangle_spec[prediction]
-            cv2.putText(frame, text, (x,y - 10), font, font_scale, color, thickness)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
-        
-        cv2.imshow('frame',frame)
-        
-        if (cv2.waitKey(1) & 0xFF) == ord('q'):
-            break
-    else:
-        break
+            total += y.size(0)
+            correct += (maximum == y).sum().item()
 
-cap.release()
-cv2.destroyAllWindows() """
+    accuracy = correct/total
+    print(f'Testing Accuracy: {accuracy}')
+
+test(loaded_model, test_loader)
+
+def test_video(model):
+    cap = cv2.VideoCapture(0)
+
+    font_scale, thickness = 1, 2
+    red,green,blue = (0,0,255), (0,255,0), (255,0,0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    rectangle_spec = {0: ("Masked", green), 1: ("No Mask", red), 2: ("Incorrect Mask", blue)}
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.4, 4)
+            
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), blue, 2)
+                
+                cropped_img = frame[y:y+h, x:x+w]
+                pil_image = Image.fromarray(cropped_img, mode = "RGB")
+                pil_image = train_transforms(pil_image)
+                image = pil_image.unsqueeze(0)
+                
+                result = loaded_model(image)
+                _, maximum = torch.max(result.data, 1)
+                prediction = maximum.item()
+                print(prediction)
+                
+                text, color = rectangle_spec[prediction]
+                cv2.putText(frame, text, (x,y - 10), font, font_scale, color, thickness)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
+            
+            cv2.imshow('frame',frame)
+            
+            if (cv2.waitKey(1) & 0xFF) == ord('q'):
+                break
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+# test_video(loaded_model)
