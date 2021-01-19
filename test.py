648cd6bac93b60c94e972d5c4f014f3c4306ec51
mabdullahsoyturk <muhammetabdullah.soyturk@agu.edu.tr>
2021-01-19 12:12:14 +0100

    add masks

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import numpy as np
+import time
 import os
 import cv2
 import glob
@@ -8,11 +10,12 @@ import torchvision.models
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torch.utils.data import random_split, DataLoader
+import torch.optim.lr_scheduler as lr_scheduler
 from utils import load_checkpoint, save_loss_fig, save_accuracy_fig
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 mean, std = [0.6583, 0.4580, 0.0877], [0.2412, 0.2313, 0.2387]
-BATCH_SIZE, LEARNING_RATE, NUM_EPOCH, WEIGHT_DECAY = 16, 1e-4, 20, 0
+BATCH_SIZE, LEARNING_RATE, NUM_EPOCH, WEIGHT_DECAY = 16, 1e-4, 20, 1e-5
 
 models = {
     'resnet18': torchvision.models.resnet18(),
@@ -31,7 +34,7 @@ def get_model(model_name):
         n_inputs = model.fc.in_features
         last_layer = torch.nn.Linear(n_inputs, 3)
         model.fc.out_features = last_layer
-    elif model_niame == 'mobilenet':
+    elif model_name == 'mobilenet':
         model.classifier._modules['1'] = torch.nn.Linear(1280, 3)
     elif "vgg" in model_name:
         model.classifier._modules['6'] = torch.nn.Linear(4096, 3)
@@ -45,9 +48,23 @@ def get_model(model_name):
     return model
 
 model = get_model("resnet18")
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+#optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
 
 train_transforms = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor(),transforms.Normalize(mean=mean,std=std)])
+augment_transforms = transforms.Compose([
+    transforms.Resize((224,224)),
+    torchvision.transforms.RandomHorizontalFlip(p=1),
+    torchvision.transforms.RandomRotation(20, resample=Image.BILINEAR),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean,std=std)
+])
 dataset = datasets.ImageFolder("train", transform = train_transforms)
+augmented_dataset = datasets.ImageFolder("train", transform = augment_transforms)
+dataset = torch.utils.data.ConcatDataset([augmented_dataset,dataset])
+# dataset = torch.utils.data.Subset(dataset, np.random.choice(len(dataset), 1000, replace=False))
 
 dataset_size = len(dataset)
 train_size, val_size = int(dataset_size * 0.6),  int(dataset_size * 0.2)
@@ -60,9 +77,6 @@ print(f'Dataset size: {dataset_size}\nTrain set size: {len(train_dataset)}\nVali
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
-
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
 
 best_epoch = 0
 
@@ -88,7 +102,7 @@ def train(model):
             correct += (maximum == y).sum().item()
         
         training_accuracy = correct / total
-        training_losses.append(epoch_train_loss)
+        training_losses.append(epoch_train_loss / total)
         training_accuracies.append(training_accuracy)
 
         epoch_val_loss, correct, total = 0,0,0
@@ -105,10 +119,10 @@ def train(model):
                 total += y.size(0)
                 correct += (maximum == y).sum().item()
                 
-        val_losses.append(epoch_val_loss)
+        val_losses.append(epoch_val_loss / total)
         accuracy = correct/total
         validation_accuracies.append(accuracy)
-        print(f'EPOCH:{epoch}, Training Loss:{epoch_train_loss}, Validation Loss:{epoch_val_loss}, Training Accuracy: {training_accuracy}, Validation Accuracy: {accuracy}')
+        print(f'EPOCH:{epoch}, Training Loss:{epoch_train_loss / total}, Validation Loss:{epoch_val_loss / total}, Training Accuracy: {training_accuracy}, Validation Accuracy: {accuracy}')
         
         if min(val_losses) == val_losses[-1]:
             best_epoch = epoch
@@ -117,11 +131,20 @@ def train(model):
             torch.save(checkpoint, "models/" + f'{epoch}.pth')
             print("Model saved")
 
-train(model)
-save_loss_fig(NUM_EPOCH, training_losses, val_losses)
-save_accuracy_fig(NUM_EPOCH, training_accuracies, validation_accuracies)
+        scheduler.step()
+        print(optimizer.state_dict()['param_groups'][0]['lr'])
 
-filepath = f'models/{best_epoch}.pth'
+    save_loss_fig(NUM_EPOCH, training_losses, val_losses)
+    save_accuracy_fig(NUM_EPOCH, training_accuracies, validation_accuracies)
+
+start_time = time.time()
+#train(model)
+end_time = time.time()
+duration = end_time - start_time
+print(f'Time it takes to train {duration}')
+
+#filepath = f'models/{best_epoch}.pth'
+filepath = f'models/{9}.pth'
 loaded_model = load_checkpoint(filepath)
 
 def test(model, test_loader):
@@ -184,5 +207,20 @@ def test_video(model):
 
     cap.release()
     cv2.destroyAllWindows()
+
+def transform_image():
+    image = Image.open("diff_mask.jpeg")
+
+    return train_transforms(image).unsqueeze(0)
+
+def get_prediction(image_tensor):
+    images = image_tensor.to(device)
+    outputs = loaded_model(images)
+    _, predicted = torch.max(outputs.data, 1)
+    return predicted
+
+tensor = transform_image()
+prediction = get_prediction(tensor)
+print(prediction.item())
 
 # test_video(loaded_model)
